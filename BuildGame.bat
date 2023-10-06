c041cb703b2aa421895b15a40dd9541c554e54ff
Herpsta <Nostalgic124@live.com>
2023-10-05 22:58:04 -0600

    Update BuildGame.bat

--- BuildGame.bat
+++ BuildGame.bat
@@ -5,6 +5,6 @@ setlocal
 "C:\Program Files\Unity\Hub\Editor\2022.3.10f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\nyxar\Zombie Survival RPG" -executeMethod BuildGame.BuildAll
 
 :: Create a .7z archive with the version number in the filename
-"C:\Program Files\7-Zip\7z.exe" a -t7z "Zombie Survival RPG_%VERSION%.7z" "./Releases/*"
+"C:\Program Files\7-Zip\7z.exe" a -t7z "./Releases/Zombie Survival RPG_%VERSION%.7z" "./Builds/*"
 
 endlocal
