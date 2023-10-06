5370c3964ee8b27a721a56de928dc70b6f4fc883
Herpsta <Nostalgic124@live.com>
2023-10-05 22:15:58 -0600

    Create BuildGame.bat

---
+++ Assets/BuildGame.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+
+:: Build the Unity project
+"C:\Program Files\Unity\Hub\Editor\2022.3.10f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\nyxar\Zombie Survival RPG" -executeMethod BuildGame.BuildAll
+
+:: Create a .7z archive with the version number in the filename
+"C:\Program Files\7-Zip\7z.exe" a -t7z "Zombie Survival RPG_%VERSION%.7z" "./Builds/*"
+
+endlocal
