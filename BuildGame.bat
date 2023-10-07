3795a4fe8943f1b21ff34674002a4b23b9223975
Herpsta <Nostalgic124@live.com>
2023-10-06 20:32:37 -0600

    Update BuildGame.bat

--- BuildGame.bat
+++ BuildGame.bat
@@ -1,20 +1,8 @@
 @echo off
 setlocal
 
-:: Set the version number
-set VERSION=0.2.4
-
-:: Build the Unity project
-"C:\Program Files\Unity\Hub\Editor\2022.3.10f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\nyxar\Zombie Survival RPG" -executeMethod BuildGame.BuildAll
-
-:: Create a .7z archive with the version number in the filename
-"C:\Program Files\7-Zip\7z.exe" a -t7z "./Releases/Zombie Survival RPG_%VERSION%.7z" "./Builds/*"
-
-endlocal
-
-
-@echo off
-setlocal
+:: Set the version number from environment variable
+set VERSION=%VERSION%
 
 :: Delete the contents of the Build and Release folders
 rmdir /s /q "C:\Users\nyxar\Zombie Survival RPG\Builds*"
@@ -24,6 +12,6 @@ rmdir /s /q "C:\Users\nyxar\Zombie Survival RPG\Releases*"
 "C:\Program Files\Unity\Hub\Editor\2022.3.10f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\nyxar\Zombie Survival RPG" -executeMethod BuildGame.BuildAll
 
 :: Create a .7z archive with the version number in the filename
-"C:\Program Files\7-Zip\7z.exe" a -t7z "Zombie Survival RPG_%VERSION%.7z" "./Releases/*"
+"C:\Program Files\7-Zip\7z.exe" a -t7z "./Releases/Zombie Survival RPG_%VERSION%.7z" "./Builds/*"
 
 endlocal
