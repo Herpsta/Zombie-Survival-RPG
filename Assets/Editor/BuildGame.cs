63282d2e37b798965ba26b5620ecb07ae142a5de
Herpsta <Nostalgic124@live.com>
2023-10-06 23:30:38 -0600

    v0.2.4
    - Hopefully got it working finally.

--- Assets/Editor/BuildGame.cs
+++ Assets/Editor/BuildGame.cs
@@ -1,35 +1,12 @@
 using UnityEngine;
 using UnityEditor;
 using System.Diagnostics;
-using System.IO;
 
 public class BuildGame
 {
     [MenuItem("Build/Build All")]
     public static void BuildAll()
     {
-        // Retrieve the version number from command-line arguments
-        string[] commandLineArgs = System.Environment.GetCommandLineArgs();
-        string version = "";
-
-        for (int i = 0; i < commandLineArgs.Length; i++)
-        {
-            if (commandLineArgs[i] == "-executeMethod" && i + 2 < commandLineArgs.Length)
-            {
-                if (commandLineArgs[i + 1] == "BuildGame.BuildAll")
-                {
-                    version = commandLineArgs[i + 2];
-                    break;
-                }
-            }
-        }
-
-        if (string.IsNullOrEmpty(version))
-        {
-            UnityEngine.Debug.LogError("Version number not provided. Build aborted.");
-            return;
-        }
-
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[]
         {
@@ -40,16 +17,14 @@ public class BuildGame
             "Assets/Scenes/SplashScreen.unity",
             "Assets/Scenes/TitleScreen.unity"
         };
-        buildPlayerOptions.locationPathName = "Builds/ZombieSurvivalRPG_v" + version + ".exe";
-
+        buildPlayerOptions.locationPathName = "Builds/ZombieSurvivalRPG.exe";
         buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptions.options = BuildOptions.None;
-
         BuildPipeline.BuildPlayer(buildPlayerOptions);
 
-        // Run the batch file
+        // Start the batch file to prompt for version number
         Process process = new Process();
-        process.StartInfo.FileName = "C:\\Users\\nyxar\\Zombie Survival RPG\\BuildGame.bat";
+        process.StartInfo.FileName = "BuildGame.bat";
         process.Start();
     }
 }
