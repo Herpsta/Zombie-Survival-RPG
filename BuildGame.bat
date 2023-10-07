88ec69ed704eca80871f1ab18355361e7788744e
Herpsta <Nostalgic124@live.com>
2023-10-06 20:28:31 -0600

    Update BuildGame.bat

--- BuildGame.bat
+++ BuildGame.bat
@@ -11,3 +11,19 @@ set VERSION=0.2.4
 "C:\Program Files\7-Zip\7z.exe" a -t7z "./Releases/Zombie Survival RPG_%VERSION%.7z" "./Builds/*"
 
 endlocal
+
+
+@echo off
+setlocal
+
+:: Delete the contents of the Build and Release folders
+rmdir /s /q "C:\Users\nyxar\Zombie Survival RPG\Builds*"
+rmdir /s /q "C:\Users\nyxar\Zombie Survival RPG\Releases*"
+
+:: Build the Unity project
+"C:\Program Files\Unity\Hub\Editor\2022.3.10f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\nyxar\Zombie Survival RPG" -executeMethod BuildGame.BuildAll
+
+:: Create a .7z archive with the version number in the filename
+"C:\Program Files\7-Zip\7z.exe" a -t7z "Zombie Survival RPG_%VERSION%.7z" "./Releases/*"
+
+endlocal
