6aa397cb064759a719f1527d8a59f32bbf2b104d
Herpsta <Nostalgic124@live.com>
2023-10-06 21:25:18 -0600

    v0.2.4

--- BuildGame.bat
+++ BuildGame.bat
@@ -12,6 +12,6 @@ rmdir /s /q "C:\Users\nyxar\Zombie Survival RPG\Releases*"
 "C:\Program Files\Unity\Hub\Editor\2022.3.10f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\nyxar\Zombie Survival RPG" -executeMethod BuildGame.BuildAll
 
 :: Create a .7z archive with the version number in the filename
-"C:\Program Files\7-Zip\7z.exe" a -t7z "./Releases/Zombie Survival RPG_%VERSION%.7z" "./Builds/*"
+"C:\Program Files\7-Zip\7z.exe" a -t7z "./Releases/ZombieSurvivalRPG_v%VERSION%.7z" "./Builds/*"
 
 endlocal
