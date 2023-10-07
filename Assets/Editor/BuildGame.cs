cc6fd7560ce9bec5263b691223da189868266243
Herpsta <Nostalgic124@live.com>
2023-10-06 23:35:14 -0600

    v0.2.4
    - Millionth times the charm!

--- Assets/Editor/BuildGame.cs
+++ Assets/Editor/BuildGame.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 using System.Diagnostics;
+using System.IO;
 
 public class BuildGame
 {
@@ -20,9 +21,9 @@ public class BuildGame
         buildPlayerOptions.locationPathName = "Builds/ZombieSurvivalRPG.exe";
         buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptions.options = BuildOptions.None;
+
         BuildPipeline.BuildPlayer(buildPlayerOptions);
 
-        // Start the batch file to prompt for version number
         Process process = new Process();
         process.StartInfo.FileName = "BuildGame.bat";
         process.Start();
