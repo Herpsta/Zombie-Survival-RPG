ae8be7508c28e4a9ded8447b80b8838410489ef6
Herpsta <Nostalgic124@live.com>
2023-10-18 22:44:49 -0600

    Moar work on Build Automation

--- Assets/Editor/BuildGame.cs
+++ Assets/Editor/BuildGame.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.IO;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
@@ -28,14 +27,38 @@ public class BuildGame : EditorWindow
 
     void BuildAll()
     {
-        // Read the current version from a file
         string versionPath = "Assets/version.txt"; // Replace with the actual path
-        string currentVersion = File.ReadAllText(versionPath);
+
+        if (!File.Exists(versionPath))
+        {
+            Debug.LogError("version.txt file does not exist at the specified path: " + versionPath);
+            return;
+        }
+
+        string currentVersion;
+        try
+        {
+            currentVersion = File.ReadAllText(versionPath);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to read version.txt file: " + e.Message);
+            return;
+        }
+
         string[] versionParts = currentVersion.Split('.');
-        int major = int.Parse(versionParts[0]);
-        int minor = int.Parse(versionParts[1]);
-        int patch = int.Parse(versionParts[2]);
-        int build = int.Parse(versionParts[3]);
+        if (versionParts.Length != 4)
+        {
+            Debug.LogError("Invalid version format in version.txt file");
+            return;
+        }
+
+        int major, minor, patch, build;
+        if (!int.TryParse(versionParts[0], out major) || !int.TryParse(versionParts[1], out minor) || !int.TryParse(versionParts[2], out patch) || !int.TryParse(versionParts[3], out build))
+        {
+            Debug.LogError("Invalid version format in version.txt file");
+            return;
+        }
 
         switch (releaseType)
         {
@@ -61,10 +84,41 @@ public class BuildGame : EditorWindow
 
         string newVersion = $"{major}.{minor}.{patch}.{build}";
 
-        // Save the new version to a file
-        File.WriteAllText(versionPath, newVersion);
+        try
+        {
+            File.WriteAllText(versionPath, newVersion);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to write to version.txt file: " + e.Message);
+            return;
+        }
 
-        System.Diagnostics.Process.Start("git", "lfs pull");
+        // Define the build path and create it if it doesn't exist
         string buildPath = Path.Combine(Application.dataPath, "..", "Builds", $"ZombieSurvivalRPG_v{newVersion}");
+        if (!Directory.Exists(buildPath))
+        {
+            Directory.CreateDirectory(buildPath);
+        }
+
+        // Define build options
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SplashScreen.unity", "Assets/Scenes/TitleScreen.unity", "Assets/Scenes/OptionsScreen.unity" };
+        buildPlayerOptions.locationPathName = Path.Combine(buildPath, "ZombieSurvivalRPG.exe"); // Replace with your game's name
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        // Perform the build
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+        else
+        {
+            Debug.LogError("Build failed: " + summary);
+        }
     }
 }
