f98dd25620a21ea2e98fb33c112a46aaf22a9302
Herpsta <Nostalgic124@live.com>
2023-10-25 14:27:51 -0600

    BOOGa

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,27 +3,71 @@ using UnityEngine.UI;
 using TMPro;
 using SimpleSQL;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 public class Settings : MonoBehaviour
 {
-    // All the existing code here...
+    [SerializeField]
+    private SimpleSQLManager dbManager;
+
+    // Placeholder classes for each type of settings
+    [System.Serializable]
+    public class GraphicsSettings
+    {
+        public int resolutionIndex;
+        public bool fullScreen;
+        // Add more graphics settings here
+    }
+
+    [System.Serializable]
+    public class AccessibilitySettings
+    {
+        public bool subtitles;
+        // Add more accessibility settings here
+    }
+
+    [System.Serializable]
+    public class SoundSettings
+    {
+        public float masterVolume;
+        // Add more sound settings here
+    }
+
+    [System.Serializable]
+    public class ControlsSettings
+    {
+        public KeyCode jumpKey;
+        // Add more controls settings here
+    }
+
+    [System.Serializable]
+    public class GameplaySettings
+    {
+        public int difficultyLevel;
+        // Add more gameplay settings here
+    }
 
     private async void Start()
     {
         try
         {
-            dbManager = GetComponent<SimpleSQLManager>();
-
             // Load settings from the database
             var settings = await Task.Run(() => dbManager.Query<SettingsModel>("SELECT * FROM Settings LIMIT 1"));
 
             if (settings.Count > 0)
             {
-                // All the existing code here...
+                // Deserialize settings from JSON
+                GraphicsSettings graphicsSettings = JsonConvert.DeserializeObject<GraphicsSettings>(settings[0].Graphics);
+                // Apply these settings to your game
+                // TODO: Implement this
+
+                // Similarly, deserialize and apply other settings
+                // TODO: Implement this
             }
             else
             {
-                // TODO: Initialize settings with default values
+                // Initialize settings with default values
+                SaveSettings();
             }
         }
         catch (System.Exception ex)
@@ -36,36 +80,49 @@ public class Settings : MonoBehaviour
     {
         try
         {
-            // TODO: Serialize settings to a string format (like JSON) before storing them in the database
+            // Serialize settings to JSON
+            string graphicsSettings = JsonConvert.SerializeObject(new GraphicsSettings
+            {
+                resolutionIndex = 0,
+                fullScreen = true
+            });
+
+            string accessibilitySettings = JsonConvert.SerializeObject(new AccessibilitySettings
+            {
+                subtitles = true
+            });
+
+            string soundSettings = JsonConvert.SerializeObject(new SoundSettings
+            {
+                masterVolume = 1.0f
+            });
+
+            string controlsSettings = JsonConvert.SerializeObject(new ControlsSettings
+            {
+                jumpKey = KeyCode.Space
+            });
+
+            string gameplaySettings = JsonConvert.SerializeObject(new GameplaySettings
+            {
+                difficultyLevel = 1
+            });
 
             // Save settings to the database
             await Task.Run(() => dbManager.Execute("UPDATE Settings SET Graphics = ?, Accessibility = ?, Sound = ?, Controls = ?, Gameplay = ?",
-                new GraphicsSettings
-                {
-                    // All the existing code here...
-                },
-                new AccessibilitySettings
-                {
-                    // All the existing code here...
-                },
-                new SoundSettings
-                {
-                    // All the existing code here...
-                },
-                new ControlsSettings
-                {
-                    // All the existing code here...
-                },
-                new GameplaySettings
-                {
-                    // All the existing code here...
-                }));
+                graphicsSettings, accessibilitySettings, soundSettings, controlsSettings, gameplaySettings));
 
-            // TODO: Update the UI to reflect the saved settings
+            // Update the UI to reflect the saved settings
+            UpdateUI();
         }
         catch (System.Exception ex)
         {
             Debug.LogError("Failed to save settings to the database: " + ex.Message);
         }
+    }
+
+    private void UpdateUI()
+    {
+        // Update your UI elements here based on the current settings
+        // TODO: Implement this
     }
 }
