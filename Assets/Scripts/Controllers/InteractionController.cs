b4ee05b4d245190bb88e1d8125e71f8a2dd39d96
Herpsta <Nostalgic124@live.com>
2023-11-03 20:51:09 -0600

    Update InteractionController.cs

--- Assets/Scripts/Controllers/InteractionController.cs
+++ Assets/Scripts/Controllers/InteractionController.cs
@@ -1,24 +1,102 @@
 using UnityEngine;
+using UnityEngine.Events;
+
+public interface IInteractable
+{
+    void Interact();
+    void StopInteract();
+}
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [Tooltip("Door open state")]
+    private bool isOpen = false;
+
+    [Tooltip("Speed of door opening/closing")]
+    public float speed = 2.0f;
+
+    public void Interact()
+    {
+        isOpen = !isOpen;
+
+        if (isOpen)
+        {
+            StartCoroutine(OpenDoor());
+        }
+        else
+        {
+            StartCoroutine(CloseDoor());
+        }
+    }
+
+    public void StopInteract()
+    {
+        // Stop the door from opening/closing
+        StopAllCoroutines();
+    }
+
+    private IEnumerator OpenDoor()
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(0, 90, 0);
+
+        for (float t = 0; t < 1; t += Time.deltaTime * speed)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, t);
+            yield return null;
+        }
+    }
+
+    private IEnumerator CloseDoor()
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(0, 0, 0);
+
+        for (float t = 0; t < 1; t += Time.deltaTime * speed)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, t);
+            yield return null;
+        }
+    }
+
+    public bool IsOpen()
+    {
+        return isOpen;
+    }
+}
+
+public class Container : MonoBehaviour, IInteractable
+{
+    // Similar to Door class, implement Interact(), StopInteract(), and IsOpen() methods here
+    // You can also implement OpenContainer() and CloseContainer() coroutines similar to OpenDoor() and CloseDoor()
+}
 
 public class InteractionController : MonoBehaviour
 {
-    [Tooltip("Tag for the door object")]
-    public string doorTag = "Door"; // Expose the door tag in the inspector for customization
+    [Tooltip("Event triggered when interaction occurs")]
+    private UnityEvent onInteract = new UnityEvent();
+
+    public void AddOnInteractListener(UnityAction action)
+    {
+        onInteract.AddListener(action);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the object collided with has the door tag
-        if (other.CompareTag(doorTag))
+        var interactable = other.GetComponent<IInteractable>();
+        if (interactable != null)
         {
-            HandleDoorInteraction();
+            interactable.Interact();
+            onInteract.Invoke();
         }
-        // TODO: Add more interaction triggers for other objects
     }
 
-    private void HandleDoorInteraction()
+    private void OnTriggerExit(Collider other)
     {
-        // TODO: Implement door opening logic here
+        var interactable = other.GetComponent<IInteractable>();
+        if (interactable != null)
+        {
+            interactable.StopInteract();
+        }
     }
-
-    // TODO: Add methods for other interactions like accessing containers, etc.
 }
